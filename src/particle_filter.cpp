175eeb89a3b283504d116b2b1cd9dbbc440d12c9
Neil Maude <neil.maude@gmail.com>
2017-05-06 20:29:23 +0100

    feat: Running version
    Doesn't get accurate enough results - too many cases where the particle
    probabilities all drop to zero

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -33,7 +33,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
   std::normal_distribution<double> dist_y(y, std[1]);           // This line creates a normal (Gaussian) distribution mean y.
   std::normal_distribution<double> dist_theta(theta, std[2]);   // This line creates a normal (Gaussian) distribution mean theta.
 
-  num_particles = 10;                                      // magic number of particles to start with
+  num_particles = 1;                                      // magic number of particles to start with
   for (int i = 0; i < num_particles; i++) {
     
     // create and add a new particle
@@ -59,6 +59,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
 
+  /*
   // Test code
   std::cout << "Prediction step" << std::endl;
   std::cout << "delta_t   = " << delta_t << std::endl;
@@ -66,7 +67,8 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
   std::cout << "yaw_rate  = " << yaw_rate << std::endl;
   std::cout << "std_pos[] = " << std_pos[0] << "," << std_pos[1] << "," << std_pos[2] << std::endl;
   // end of test code
-  
+  */
+
   /*
     *** What elements of the calculation require sensor noise?  
     3 standard deviations have been passed in std_pos[], which suggests adding noise to x,y,yaw
@@ -89,9 +91,11 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
   // loop over particles in particles vector, update position, add noise
   for (int i = 0; i < particles.size(); i++) {
     
+    /*
     // test code
     std::cout << "Particle ID: " << particles[i].id << std::endl;                         
     std::cout << "Previous x, y, theta : " << particles[i].x << "," << particles[i].y << "," << particles[i].theta << std::endl;
+    */
 
     // each particle has a current x,y and bearing - use these in the bicycle motion model, along with provided vel and yaw_rate
 
@@ -107,8 +111,10 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
     particles[i].y = particles[i].y + (v_over_yaw * (cos(particles[i].theta) - cos(theta_f))) + noise_y;    // y position calc
     particles[i].theta = theta_f + noise_theta;                                                             // update theta
 
+    /*
     // test code
     std::cout << "Updated x, y, theta : " << particles[i].x << "," << particles[i].y << "," << particles[i].theta << std::endl;
+    */
 
   }
   std::cout << std::endl;
@@ -125,16 +131,21 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
   // The observations is the observations list, translated into map co-ordinates
   // The aim of this function is to set the .id value for each observations entry to the .id of the nearest map point
 
+  /*
   // test code
   std::cout << "dataAssociation, " << "Landmarks : " << predicted.size() << ", Observations : " << observations.size() << std::endl;
- 
+  */
+
   // for each observation, scan over the landmarks and see which is the nearest
   for (int i = 0; i < observations.size(); i++) {
 
     double best_distance = DBL_MAX;
+    
+    /*
     // test code
     std::cout << "Initial best distance : " << best_distance << std::endl;
     // end test code
+    */
 
     for (int j = 0; j < predicted.size(); j++) {
 
@@ -149,9 +160,12 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
         observations[i].id = predicted[j].id;
         best_distance = e_distance;
 
+        /*
         // test code
         std::cout << "New best distance : " << best_distance << ", id = " << observations[i].id <<  std::endl;
         // end test code
+        */
+
       }
 
     }
@@ -174,20 +188,24 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
   //   for the fact that the map's y-axis actually points downwards.)
   //   http://planning.cs.uiuc.edu/node99.html
 
+  /*
   // test code
   std::cout << "UpdateWeights" << std::endl;
   std::cout << "Sensor range  : " << sensor_range << std::endl;
   std::cout << "Observations  : " << observations.size() << std::endl;
   std::cout << "Map landmarks : " << map_landmarks.landmark_list.size() << std::endl;
   // end of test code
+  */
 
   for (int i = 0; i < particles.size(); i++) {
     // update weights for the i-th particle
 
+    /*
     // test code
     std::cout << "Particle #" << i << std::endl;
     std::cout << "Current weight : " << particles[i].weight << std::endl;
     // end of test code
+    */
 
     // find the nearest neighbour for each measurement
     // first create a vector of observations in map co-ordinates
@@ -201,10 +219,12 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
     for (int j = 0; j < observations.size(); j++) {
       // loop over the observations
 
+      /*
       // test code
       std::cout << "Observation #" << j << std::endl;
       std::cout << "id,x,y = " << observations[j].id << "," << observations[j].x << "," << observations[j].y << std::endl;
       // end of test code
+      */
 
       // now convert this measurement to map co-ordinates
       LandmarkObs new_p;
@@ -213,9 +233,11 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
       new_p.y = particle_y + (observations[j].y * sin_theta) + (observations[j].y * cos_theta);
       obs_map.push_back(new_p);                                                                   // put this observation on the obs_map vector
 
+      /*
       // test code
       std::cout << "New observation id,x,y = " << new_p.id << "," << new_p.x << "," << new_p.y << std::endl;
       // end of test code
+      */
 
     }
 
@@ -232,11 +254,13 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
       if (e_distance < sensor_range) {                                        // if within range, use this landmark
 
+        /*
         // test code
         std::cout << "Distance to landmark = " << e_distance << " particle = " << particle_x << "," << particle_y;
         std::cout << " landmark #" << map_landmarks.landmark_list[k].id_i << " = " << map_landmarks.landmark_list[k].x_f;
         std::cout << "," << map_landmarks.landmark_list[k].y_f << std::endl;
         // end test code
+        */
 
         LandmarkObs new_l;
         new_l.id = map_landmarks.landmark_list[k].id_i;
@@ -261,12 +285,14 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
     }
 
 
+    /*
     // test code
     for (int k = 0; k < obs_map.size(); k++) {
       // print out the nearest landmarks list
       std::cout << "Observation #" << k << " set to landmark #" << obs_map[k].id << std::endl;
     }
     // end test code
+    */
 
     // update the weight
     // for each observation for this particle, calculate the multivariate Gaussian
@@ -294,9 +320,11 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
     particles[i].weight = new_weight;
 
+    /*
     // test code
     std::cout << "Particle #" << i << ", Updated weight : " << particles[i].weight << std::endl;
     // end of test code
+    */
 
     // Note: the p_xy terms can be very small for unlikely observations
 
@@ -307,25 +335,39 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
   for (int i = 0; i < particles.size(); i++) {
     normaliser += particles[i].weight;
   }
+  
+  /*
   // test code
   std::cout << "Normaliser value = " << normaliser << std::endl;
   // end test code
+  */
+
   if (normaliser != 0.0) {
     for (int i = 0; i < particles.size(); i++) {
       particles[i].weight = particles[i].weight / normaliser;
+      
+      /*
       // test code
       std::cout << "Normalised particle weight = " << particles[i].weight << " (particle id=" << i << ")" << std::endl;
       // end test code
+      */
+
     }
   }
   else {
     // zero normaliser - all particle weights must have collapsed to zero...
+    
     // test code
     std::cout << "All particles have zero weight!" << std::endl;
     // end of test code
     
-    // might want to set all of the particle weights to the uniform distribution here (i.e. max uncertainty)?
-
+    // might want to set all of the particle weights to the uniform distribution here (i.e. max uncertainty)
+    // allows the algorith to continue
+    // maybe even want to re-initialise?
+    // however, don't want this to ever happen in practice, as we now have no information about where the vehicle is located
+    for (int i = 0; i < particles.size(); i++) {
+      particles[i].weight = 1.0 / particles.size();
+    }
   }
   
 }
@@ -335,6 +377,37 @@ void ParticleFilter::resample() {
 	// TODO: Resample particles with replacement with probability proportional to their weight. 
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+
+  std::default_random_engine gen;                               // create a random number engine
+  
+  // set up the weights for the discrete distribution
+  std::vector<double> weights;
+  for (int i = 0; i < particles.size(); i++) {
+    weights.push_back(particles[i].weight);
+  }
+
+  std::discrete_distribution<> discrete_sample(weights.begin(), weights.end());     // set up the discrete distribution
+
+  int n_sample;
+
+  // need to make a copy of the particles list, from which to copy back as samples are made
+  std::vector<Particle> old_particles;
+  old_particles = particles;              // be very surprised if it's that easy!!!
+
+  for (int i = 0; i < particles.size(); i++) {
+    // sample a particle from the distribution
+    n_sample = discrete_sample(gen);
+
+    /*
+    // test code
+    std::cout << "Sampled particle #" << n_sample << " - which had prob: " << old_particles[n_sample].weight << std::endl;
+    //
+    */
+
+    // copy the particle from old_particles over this particle in particles
+    particles[i] = old_particles[n_sample];   // again, can't be this easy can it?
+
+  }
 
 }
 
